3cca910d7acda58f5e7433816d0f07c1da529fef
miquel <miquel.angel.f.t@gmail.com>
2015-06-28 20:27:00 +0200

    pass the test for draw only one point each team

--- jasmine js/src/Tennis.js
+++ jasmine js/src/Tennis.js
@@ -1,6 +1,12 @@
 function Tennis() {
+	this.player_message = "Player A 0 - Player B 0"
 }
 
+Tennis.prototype.PLAYER_A = 1;
+Tennis.prototype.PLAYER_B = 2;
 Tennis.prototype.getScore = function() {
-	return "Player A 0 - Player B 0";
+	return this.player_message;
+};
+Tennis.prototype.score = function(player) {
+	this.player_message = "Player A 15 - Player B 15"
 };
