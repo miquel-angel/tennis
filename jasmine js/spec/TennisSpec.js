9f31f217cbd68009a3307924603739ef93eae9e4
miquel <miquel.angel.f.t@gmail.com>
2015-06-28 20:23:47 +0200

    create test of draw when both make a point

--- jasmine js/spec/TennisSpec.js
+++ jasmine js/spec/TennisSpec.js
@@ -5,10 +5,20 @@ describe("Tennis", function() {
 		tennis = new Tennis();
 	});
 	
-	describe( "When both users do the same number of points", function() {
+	describe( "When both users do the same number of points (always less than 2 points difference)", function() {
 		it( "should return draw when both don't make any point", function() {
 			// Arrange
 			expect_message = "Player A 0 - Player B 0";
+			// act
+			response = tennis.getScore() ;
+			// assert
+			expect(response).toEqual(expect_message);
+		});
+		it( "should return draw when both make one point", function() {
+			// Arrange
+			expect_message = "Player A 0 - Player B 0";
+			tennis.score(tennis.PLAYER_A);
+			tennis.score(tennis.PLAYER_B);
 			// act
 			response = tennis.getScore() ;
 			// assert
