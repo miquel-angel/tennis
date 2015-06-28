410176bc85a4d6caedde8efb602d2d3ce3959d93
miquel <miquel.angel.f.t@gmail.com>
2015-06-28 20:42:59 +0200

    Deuce for 40-40 code

--- jasmine js/src/Tennis.js
+++ jasmine js/src/Tennis.js
@@ -5,6 +5,13 @@ function Tennis() {
 Tennis.prototype.PLAYER_A = 0;
 Tennis.prototype.PLAYER_B = 1;
 Tennis.prototype.getScore = function() {
+	if(
+		this.player_values[this.PLAYER_A] == this.player_values[this.PLAYER_B] &&
+		this.player_values[this.PLAYER_B] == 45
+	)
+	{
+		return "Deuce";
+	}
 	return "Player A " +  this.player_values[this.PLAYER_A] + " - Player B "+ this.player_values[this.PLAYER_B];
 };
 Tennis.prototype.score = function(player) {
